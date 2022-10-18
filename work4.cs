c4b6b74813b9ce92c11534ffdcf2028d57dfa073
GrigoriyLeb <114830136+GrigoriyLeb@users.noreply.github.com>
2022-10-18 23:12:25 +0300

    Упражнение 4

---
+++ work4.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+int NewCount = 2;
+while (NewCount <= A)
+{
+Console.Write($"{NewCount}" );
+NewCount += 2;
+}
+
+
