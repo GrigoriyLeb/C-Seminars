734d43ccff365f8e3434058f6f70142f50773f91
GrigoriyLeb <114830136+GrigoriyLeb@users.noreply.github.com>
2022-10-24 16:07:13 +0300

    Задача 15
    Я пока не понял как можно сделать условие не для каждого дня. Искать подсказку в интернете не стал.

---
+++ Work 2-3.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+
+Console.Write("Введите номер дня недели ");
+int dayOfWeek = int.Parse(Console.ReadLine()!);
+
+if (dayOfWeek > 5)
+{
+    Console.Write("Это выходной! Наслаждайтесь отдыхом. ");
+}
+else
+{
+    Console.Write("Это будний день, идите на работу! ");
+}
+
+if (dayOfWeek == 1)
+{
+    Console.Write("Это понедельник!");
+}
+else if (dayOfWeek == 2)
+{
+    Console.Write("Это вторник!");
+}
+else if (dayOfWeek == 3)
+{
+    Console.Write("Это среда!");
+}
+else if (dayOfWeek == 4)
+{
+    Console.Write("Это четверг!");
+}
+else if (dayOfWeek == 5)
+{
+    Console.Write("Это пятница!");
+}
+else if (dayOfWeek == 6)
+{
+    Console.Write("Это суббота!");
+}
+else
+{
+    Console.Write("Это воскресенье!");
+}
