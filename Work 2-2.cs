2617cbfc5bc88fca9effb1cbabff788f89352245
GrigoriyLeb <114830136+GrigoriyLeb@users.noreply.github.com>
2022-10-24 15:34:27 +0300

    Задача 13

---
+++ Work 2-2.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру (справа налево) заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+int num = new Random().Next(1, 999);
+
+if (num > 99)
+{
+    int numberthree = (num % 10);
+    Console.WriteLine($"Наше число {num}, а третья цифра числа {numberthree}");
+}
+
+else
+{
+    Console.WriteLine($"Наше число {num} и в нем нет третьей цифры");
+}
